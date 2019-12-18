999d097a574119aa250dd3592f362040d0bf1e9e
cleverfang <512628189@qq.com>
2019-12-18 16:05:00 +0800

    工程初始化

---
+++ film/src/test/java/com/dmf/film/FilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmf.film;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
